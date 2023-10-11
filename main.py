088186ae8b9be5d19ff20cffa5c34fd2d36517f6
Digin <xanmanxan@outlook.com>
2023-10-11 02:51:29 -0500

    verbump

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from core.executor import run
 from colors import colors
 
 if __name__ == '__main__':
-    print(f"{colors.CYAN}fuse {colors.BOLD}0.0.1a1{colors.RESET}")
+    print(f"{colors.CYAN}fuse {colors.BOLD}0.0.1a2{colors.RESET}")
     while True:
         text = input(f"{colors.RESET}fuse > {colors.GREEN}")
         result, error = run("<program entry>", text)
